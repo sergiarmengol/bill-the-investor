81cc85e9eeb87308e4e2140ff3f4e32f7c0d941b
Sergi Armengol <sergiarmengol@macbook-pro-de-sergi.home>
2017-06-22 18:41:22 +0200

    Checking everything is well uploaded

--- js/script.js
+++ js/script.js
@@ -100,10 +100,9 @@ $('#newExchangeButton').click(function(){
       },
       success:  function (response) {
         if(response) {
-            var res = JSON.parse(response)
-            console.log(res);
-            var content = '<tr><td width="50%">'+res.data.name+'</td><td width="10%">';
-            if(res.data.state == 1) {
+            console.log(response);
+            var content = '<tr><td width="50%">'+response.data.name+'</td><td width="10%">';
+            if(response.data.state == 1) {
               content += '<span class="label label-success">Active</span></td></tr>';
             } else {
               content += '<span class="label label-danger">Desactive</span></td></tr>';
